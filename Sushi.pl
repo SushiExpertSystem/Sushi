15530e17887043bd42babc8fac7a1a4f9f63e82f
Lynn-Ngx <C15426672@mydit.ie>
2018-03-22 15:18:16 +0100

    Lynn Dau

--- Sushi.pl
+++ Sushi.pl
@@ -5,7 +5,7 @@ go :- hypothesize(Sushi),
 
 % hypothesize to be tested COB
 hypothesize(tiger_roll) :- tiger_roll, !.
-hypothesize(philadelphia_Roll) :- philadelphia_Roll, !.
+hypothesize(philadelphia_roll) :- philadelphia_roll, !.
 hypothesize(crunch_roll) :- crunch_roll, !.
 hypothesize(dynamite_roll) :- dynamite_roll, !.
 hypothesize(california_roll) :- california_roll, !.
@@ -27,6 +27,25 @@ hypothesize(salmon) :- salmon, !.
 hypothesize(unkown). % no diagnosis
 
 % sushi identification rules
+tiger_roll :- fish, verify(avocado), verify(cucumber), verify(tobiko), verify(rice), verify(seaweed).
+tamango :- veggie, verify(egg), verify(seaweed).
+maguro :- fish, verify(tuna), verify(rice).
+philadelpha_roll :- fish, verify(salmon), verify(avocado), verify(cream_cheese), verify(rice), verify(seaweed).
+crunch_roll :- fish, verify(spice), verify(tuna), verify(tempura), verify(rice), verify(seaweed).
+dynamite_roll :- fish, verify(tempura), verify(bean_sprout), verify(carrots), verify(avocado), verify(cucumber), verify(spice), verify(mayo), verify(rice), verify(seaweed).
+california_roll :- fish, verify(crab), verify(avocado), verify(cucumber), verify(sesame_seeds), verify(rice), verify(seaweed).
+spicy_tuna_roll :- fish, verify(tuna), verify(mayo), verify(spice), verify(rice), verify(seaweed).
+caterpillar_roll :- fish, verify(eel), verify(cucumber), verify(avocado), verify(rice), verify(seaweed).
+spider_roll :- fish, verify(crab), verify(tempura), verify(cucumber), verify(avocado), verify(spice), verify(mayo), verify(rice), verify(seaweed).
+vegetable_roll :- veggie, verify(cucumber), verify(carrots), verify(scallion), verify(asparagus), verify(cream_cheese), verify(rice), verify(seaweed).
+shrimp_tempura_roll :- fish, verify(shrimp), verify(tempura), verify(avocado), verify(rice), verify(seaweed).
+surf_and_turf_roll :- fish, verify(crab), verify(cucumber), verify(avocado), verify(rice), verify(seaweed), verify(carrots), verify(tuna), verify(salmon).
+tempura_roll :- fish, verify(tempura), verify(rice), verify(seaweed).
+sake :- fish, verify(salmon), verify(rice).
+ebi :- fish, verify(prawn), verify(rice).
+unagi :- fish, verify(eel), verify(rice), verify(seaweed).
+katsuo :- fish, verify(tuna).
+salmon :- fish, verify(salmon).
 
 % calssification rules, Q to be asked COB
 veggie :- verify(meat), !.
